aa3383b74fc3786286c8e78df36480679f9d15df
SeDan2004 <76280185+SeDan2004@users.noreply.github.com>
2021-02-21 20:12:27 +0300

    add

--- Scripts/DeleteUser.php
+++ Scripts/DeleteUser.php
@@ -12,13 +12,17 @@ array_shift($ArrUser); array_shift($ArrUser);
 
 for ($i = 0; $i <= count($ArrUser) - 1; $i++) {
 
+
   if (is_dir($dir . '/' . $ArrUser[$i])) {
 
-    $User_Game = scandir($dir . '/' . $ArrUser[$i]);
+$User_Game = scandir($dir . '/' . $ArrUser[$i]);
+
 
 array_shift($User_Game); array_shift($User_Game);
 
+
 $m = 0;
+
 
     while ($m <= count($User_Game) - 1) {
 
@@ -32,11 +36,12 @@ rmdir($dir . '/' . $ArrUser[$i]);
 } else {
 
 unlink($dir . '/' . $ArrUser[$i]);
-rmdir($dir)
 
 }
 
 }
+
+rmdir($dir);
 
           }
 
